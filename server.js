4183ec5ceed5c25556b49c7d95c363e17c66050c
lakith <lakith1995@gmail.com>
2018-05-30 17:08:36 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname+'/views/partials');
+app.set('view engine','hbs');
+
+hbs.registerHelper('getCurrentYear',()=>{
+    return new Date().getFullYear();
+});
+hbs.registerHelper('convertUpper',(text)=>{
+    return text.toUpperCase();
+});
+
+app.use((req,res,next)=>{
+
+var time = new Date().toString();
+next();
+var printlog = 'Access time - '+time+' | Request method - '+req.method+' | Request URL - '+req.url;
+console.log(printlog);
+fs.appendFileSync('server.log',printlog+'\n');
+
+});
+
+app.use((req,res,next)=>{
+    res.render('maintance.hbs');
+});
+
+app.use(express.static(__dirname+'/public'));
+
+app.get('/',(req,res)=>{
+// res.send('<h2> Page is displaing with express </h2>');
+//    res.send({
+//        name:'lakith',
+//        likes:[
+//            'biking',
+//            'swimming',
+//            'computing'
+//        ]
+//    });
+
+res.render('home.hbs',{
+    welcome:'Welcome to my first node web suite',
+    pageTitle:'Home page',
+    year: new Date().getFullYear()
+});
+
+});
+
+app.get('/about',(req,res)=>{
+
+    //res.send('<h3> This is the about us page </h3>');
+    res.render('about.hbs',{
+        pageTitle:'About us page',
+        year: new Date().getFullYear()
+    });
+
+});
+
+app.get('/bad',(req,res)=>{
+
+    res.send({
+        error:'bad request send'
+    });
+
+});
+app.listen(3000,()=>{
+    console.log('Server is up on port 3000');
+});
